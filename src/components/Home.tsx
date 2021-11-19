89551d807b796f44b4fa70d9b6b38f5165b7db4b
mimichloee <mimichloee@gmail.com>
2021-11-19 21:06:43 +0900

    feat: example code for session storage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const SESSION_KEY = 'uniqueId';
+
 const Home = () => {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const getUniqueId = window.sessionStorage.getItem(SESSION_KEY);
+    const isNewTab = searchParams.get('newTab');
+
+    if (!getUniqueId || isNewTab === 'true') {
+      window.sessionStorage.setItem(SESSION_KEY, new Date().getTime().toString());
+    }
+
+    console.log('uniqueId', window.sessionStorage.getItem(SESSION_KEY));
+  }, [searchParams]);
+
   return <div>Home page</div>;
 };
 
